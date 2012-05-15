27a00b00bc84865c177ffbce450b00a414745057
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2012-05-16 02:08:05 +0300

    added entropy information and corrected style

--- anapass/anapass.py
+++ anapass/anapass.py
@@ -5,15 +5,16 @@
 # Author: Kimmo Brunfeldt
 # 2011
 
-import hashlib
 import time
 import string
 import logger
 import sys
+import math
 
 # How many hashes can be generated in second
-HASH_PER_SEC = 10**9 * 30 # 30 milliard
-# http://hashcat.net/oclhashcat-plus/#tested-gpu -> With 8 GPUs this cracking speed is possible.
+HASH_PER_SEC = (10 ** 9) * 30   # 30 milliard
+# http://hashcat.net/oclhashcat-plus/#tested-gpu
+# -> With 8 GPUs this cracking speed is possible.
 
 # That's why it is being multiplied by 5 (I bet a group of crackers can do
 # even better)
@@ -34,23 +35,24 @@ OK_SECS = DAY * 365 * 10                 # 10 years
 STRONG_SECS = DAY * 365 * 1000           # 1000 years
 VERYSTRONG_SECS = DAY * 365 * 10 ** 25   # 10^25 years
 
+
 def secs_readable(secs):
     """Converts seconds to human-readable form."""
 
     if secs > YEAR:
         return str( int(round(float(secs) / YEAR)) ) + " years"
-    
+
     if secs > DAY:
         return str( int(round(float(secs) / DAY)) ) + " days"
-    
+
     if secs > HOUR:
         return str( int(round(float(secs) / HOUR)) ) + " hours"
-    
+
     if secs > MINUTE:
         return str( int(round(float(secs) / MINUTE)) ) + " minutes"
-    
+
     return str(secs) + "seconds"
-    
+
 
 class PasswordOracle(object):
     """Analyzes passwords' weakness."""
@@ -59,7 +61,7 @@ class PasswordOracle(object):
 
         self.log = logger.Logger(time.time())
         self.read_wordlists(wordlist_files)
-        
+
         if len(self.password_list) > 0:
             self.modify_wordlist()
             self.log.log('Sorting wordlist.. ')
@@ -70,67 +72,79 @@ class PasswordOracle(object):
         """Read wordlists to memory"""
 
         self.password_list = []
-        
+
         self.log.log('Reading wordlist files..')
-        
+
         for filename in wordlist_files:
             self.password_list += open(filename).read().splitlines()
-        
-        self.log.log('%s words read.'%len(self.password_list))
-    
+
+        self.log.log('%s words read.' % len(self.password_list))
+
     def modify_wordlist(self):
-        
+
         self.log.log('Modifying wordlists. Adding ! to end.')
-        self.password_list += map(lambda x: x + '!', self.password_list)
-        
+        self.password_list += [x + '!' for x in self.password_list]
+
         self.log.log('Changing o -> 0 and i -> 1.')
-        self.password_list += map(lambda x: x.replace('o','0').replace('i','1'),
-                                   self.password_list)
-        
-        self.log.log('Wordlist contains now %s words.' % len(self.password_list))
-        
+        self.password_list = [x.replace('o', '0').replace('i', '1')
+                              for x in self.password_list]
+
+        self.log.log('Wordlist contains now %s words.' %
+                     len(self.password_list))
+
     def combinations(self, charsetlen, passwordlen):
         """Returns the amount of different passwords with a certain
         character set and password length. If password's length is 3,
         1 - 3 length password combinations is returned."""
-        
+
         # charsetlen = 26, passlen = 3 returns: 26**1 + 26**2 + 26**3
         return sum([charsetlen ** (x + 1) for x in xrange(passwordlen)])
 
     def is_known_word(self, password):
         """Checks if password is a common word in wordlists"""
-        
-        if password in self.password_list:
-            return True
-        
-        return False
-    
+        return password in self.password_list
+
+    def count_entropy(self, charsetlen, passwordlen):
+        """Counts entropy of a password based on formula found from
+        http://en.wikipedia.org/wiki/Password_strength"""
+        entropy = passwordlen * math.log(charsetlen) / math.log(2)
+        return int(math.ceil(entropy))
+
+
     def give_grade(self, password):
         """Analyze the strength of password against bruteforce.
         Returns a word to describe password"""
-        
+
         charsetlen = self.charset_len(password)
-        
+
         all_combs = self.combinations(charsetlen, len(password))
         all_secs = all_combs / HASH_PER_SEC
-        
-        self.log.log('Charset\'s length is %s.'%charsetlen)
 
-        self.log.log('1 - %s length passwords\' combinations: %s.'%(len(password), all_combs))
-        self.log.log('1 - %s length\'s bruteforcing time: ~%s'%(len(password), secs_readable(all_secs)))
- 
+        entropy = self.count_entropy(charsetlen, len(password))
+
+        self.log.log('Charset\'s length is %s.' % charsetlen)
+
+        self.log.log('Entropy of password is %s bits. 2^%s = %s(entropy based combinations)' % (entropy, entropy, 2 ** entropy))
+
+        self.log.log('1 - %s length passwords\' combinations: %s.' %
+                     (len(password), all_combs))
+
+        self.log.log('On average, half of the combinations have to be tested to crack the password.')
+        self.log.log('1 - %s length\'s bruteforcing time: ~%s' %
+                     (len(password), secs_readable(all_secs / 2)))
+
         if all_secs > VERYSTRONG_SECS:
             grade = "very strong"
-        
+
         elif all_secs > STRONG_SECS:
             grade = "strong"
-        
+
         elif all_secs > OK_SECS:
             grade = "just ok"
-        
+
         elif all_secs > WEAK_SECS:
             grade = "weak"
-        
+
         else:
             grade = "useless"
 
@@ -138,56 +152,62 @@ class PasswordOracle(object):
             if self.is_known_word(password):
                 grade = "useless(COMMON WORD, WAS FOUND IN WORDLIST!)"
 
-        self.log.log('"%s" = %s'%(password, grade.upper()))
-    
+        self.log.log('"%s" = %s' % (password, grade.upper()))
+
     def charset_len(self, password):
         """Returns integer of password charsetlen"""
-        
+
         charsets = [
-            string.ascii_lowercase,            # len=26
-            string.ascii_uppercase,            # len=26
-            string.digits,                     # len=10
-            "!?",                              # len=2
-            "-.%*'\"#$@/&()=[]\,+_:;<>^`{|}~ ",# len=31
-            "öäå",                             # len=3
+            string.ascii_lowercase,              # len=26
+            string.ascii_uppercase,              # len=26
+            string.digits,                       # len=10
+            "!?",                                # len=2
+            "-.%*'\"#$@/&()=[]\,+_:;<>^`{|}~ ",  # len=31
+            "öäå",                               # len=3
         ]
-        
-        used_sets = [False] * len(charsets)  # To determine already added sets.
-        
+        # To determine already added sets.
+        used_sets = [False] * len(charsets)
+
         self.log.log('Charset is: ', False)  # Without endline char.
         charsetlen = 0
-        
+
         # Add charsets to charsetlen.
         # Sort password's characters -> charsets are printed in same order.
         for char in ''.join(sorted(password))[::-1]:
             for index, charset in enumerate(charsets):
-                
+
                 if char in charset and not used_sets[index]:
                     charsetlen += len(charset)
                     used_sets[index] = True
-                    
+
                     self.log.stdout(charset)
-        
-        print('') 
-        
+
+        print('')
+
         return charsetlen
-        
+
     def analyze_all(self, words):
         """Analyze strength of words against bruteforce and wordlists"""
-        
+
         print('')
         for word in words:
+            if not len(word):
+                continue
 
-            self.log.log('Analyzing password "%s" (length = %s)'% (word, len(word)))
-            self.give_grade(word)    
-            
+            self.log.log('Analyzing password "%s" (length = %s)' %
+                         (word, len(word)))
+            self.give_grade(word)
+
             print('')
 
 if __name__ == '__main__':
 
+    if len(sys.argv) < 2:
+        print('Usage: ./anapass.py passwords_to_analyze.txt [wordlist1.txt] ... [wordlistN.txt]')
+        sys.exit(1)
+
     print('Calculations are based on following estimates:')
-    print('%s hashes/sec is cracking speed.'% HASH_PER_SEC)
+    print('%s hashes/sec is cracking speed.' % HASH_PER_SEC)
 
     p = PasswordOracle(sys.argv[2:])
     p.analyze_all(open(sys.argv[1]).read().splitlines())
-    
